9b9da000ffe7c745db7a69c742afd7c3d40a19f7
Lucas Alves <lucas.alves@championx.com>
2021-06-04 21:27:06 -0300

    add files

---
+++ src/EventsManagerPersistence/cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type result struct {
+	data string
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+
+		content, _ := json.Marshal(&result{
+			data: "OK",
+		})
+		w.WriteHeader(200)
+		w.Write(content)
+
+	}).Methods("GET")
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
---
+++ src/EventsManagerPersistence/go.mod
@@ -0,0 +1,5 @@
+module eventsmanagerpersistence
+
+go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
